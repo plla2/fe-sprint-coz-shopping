d2ac07da9033568f3cee038419316e6d097de9a1
plla2 <jhbljs92@naver.com>
2023-05-18 16:09:40 +0900

    토스트, 무한스크롤 제외 완성

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 import axios from 'axios';
 
 function App() {
-  const bookmarkRender = JSON.parse(localStorage.getItem("bookmark"));
+  const bookmarkRender = JSON.parse(localStorage.getItem("bookmark")) || [];
   const [bookmarks, setBookmarks] = useState(bookmarkRender)
   const [res2, setRes2] = useState([])
   const [filter, setFilter] = useState("every")
@@ -36,7 +36,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Main bookmarks={bookmarks} setBookmarks={setBookmarks} />} />
           <Route path="/products/list" element={<CartListPage filter={filter} setFilter={setFilter} setBookmarkState={setBookmarks} itemList2={res2} setRes2={setRes2} />} />
-          <Route path="/bookmark" element={<BookMarkPage filter={filter} setFilter={setFilter} />} />
+          <Route path="/bookmark" element={<BookMarkPage filter={filter} setFilter={setFilter} setBookmarks={setBookmarks} />} />
         </Routes>
         <Footer />
       </div>
--- src/pages/CartListPage.js
+++ src/pages/CartListPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import classes from "./CartListPage.module.css"
-import bookmark from "../assets/Property 1=off.png"
 import Filter from '../components/Filter'
+import Card from '../components/Card'
 
 const CartListPage = ({ itemList2, setBookmarkState, filter, setFilter, setRes2 }) => {
   const handleBookmark = (item) => {
@@ -18,110 +18,15 @@ const CartListPage = ({ itemList2, setBookmarkState, filter, setFilter, setRes2 
     localStorage.setItem("bookmark", JSON.stringify(updatedBookmark));
     setBookmarkState(updatedBookmark)
   }
-  console.log("itemList2", itemList2)
+
   return (
     <>
       <Filter setRes2={setRes2} itemList2={itemList2} filter={filter} setFilter={setFilter} />
       <ul className={classes.itemList}>
         {itemList2.map(item => {
-          switch (item.type) {
-            case "Product":
-              return (
-                <li className={classes.item} key={item.id}>
-                  <span className={classes.imgBox}>
-                    <img
-                      className={classes.img}
-                      src={item.image_url}
-                      alt={item.title}
-                    />
-                    <img
-                      className={classes.bookmark}
-                      src={bookmark}
-                      alt='bookmark'
-                      onClick={() => handleBookmark(item)}
-                    />
-                  </span>
-                  <span className={classes.firstLine}>
-                    <span className={classes.title}>{item.title}</span>
-                    <span className={classes.percent}>
-                      {item.discountPercentage}%
-                    </span>
-                  </span>
-                  <span className={classes.follower}>
-                    {item.price
-                      .toString()
-                      .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
-                    원
-                  </span>
-                </li>
-              );
-            case "Category":
-              return (
-                <li className={classes.item} key={item.id}>
-                  <span className={classes.imgBox}>
-                    <img
-                      className={classes.img}
-                      src={item.image_url}
-                      alt={item.title}
-                    />
-                    <img
-                      className={classes.bookmark}
-                      src={bookmark}
-                      alt='bookmark'
-                      onClick={() => handleBookmark(item)}
-                    />
-                  </span>
-                  <span className={classes.Catetitle}>#{item.title}</span>
-                </li>
-              );
-            case "Exhibition":
-              return (
-                <li className={classes.item} key={item.id}>
-                  <span className={classes.imgBox}>
-                    <img
-                      className={classes.img}
-                      src={item.image_url}
-                      alt={item.title}
-                    />
-                    <img
-                      className={classes.bookmark}
-                      src={bookmark}
-                      alt='bookmark'
-                      onClick={() => handleBookmark(item)} />
-                  </span>
-                  <span className={classes.title}>{item.title}</span>{" "}
-                  {item.sub_title}
-                </li>
-              );
-            case "Brand":
-              return (
-                <li className={classes.item} key={item.id}>
-                  <span className={classes.imgBox}>
-                    <img
-                      className={classes.img}
-                      src={item.brand_image_url}
-                      alt={item.brand_name}
-                    />
-                    <img
-                      className={classes.bookmark}
-                      src={bookmark}
-                      alt='bookmark'
-                      onClick={() => handleBookmark(item)} />
-                  </span>
-                  <span className={classes.firstLine}>
-                    <span className={classes.title}>{item.brand_name}</span>
-                    <span className={classes.percent}>관심고객수</span>{" "}
-                  </span>
-                  <span className={classes.follower}>
-                    {item.follower
-                      .toString()
-                      .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
-                  </span>
-                </li>
-              );
-            default:
-              return null;
-          }
+          return (
+            <Card item={item} key={`${item.id}`} handleBookmark={handleBookmark} />
+          )
         })}
       </ul>
     </>
